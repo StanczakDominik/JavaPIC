d7abe838a7dfd4d812fdaec735baa25c0187e60e
Dominik <stanczakdominik@gmail.com>
2015-05-03 09:54:47 +0200

    Grid improved further
    Signed-off-by: Dominik <stanczakdominik@gmail.com>

--- JavaPIC/src/pic/Grid.java
+++ JavaPIC/src/pic/Grid.java
@@ -3,6 +3,10 @@ package pic;
 public class Grid {
 
 	String name;
+	double[] eField;
+	double[] density;
+	double[] frequencies;
+	double[] potential;
 	public Grid() {
 		// TODO Auto-generated constructor stub
 	}
