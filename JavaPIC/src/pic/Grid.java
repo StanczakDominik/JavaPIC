2b77914f573d1928b7494872a9c0db479adce668
Dominik <stanczakdominik@gmail.com>
2015-05-03 18:31:05 +0200

    Removed unnecessary code; added charge density interpolation

--- JavaPIC/src/pic/Grid.java
+++ JavaPIC/src/pic/Grid.java
@@ -17,10 +17,51 @@ public class Grid {
 	
 	public int getIndexOnGrid(double position)
 	{
-		return (int)(Math.floor(position/this.gridStep));
+		return (int)(Math.floor(position/gridStep));
 	}
+	
+	public void update(Species[] listofspecies)
+	{
+		int index;
+		density = new double[gridPointNumber];
+		double[] speciesDensity = new double[gridPointNumber];
+		
+		//calculate density
+		for (Species species:listofspecies)
+		{
+			for (int i=0; i<species.numberOfParticles; i++)
+			{
+				index = getIndexOnGrid(species.position[i]);			
+				if (index<gridPointNumber-1)
+				{
+					speciesDensity[index]+=1-(species.position[i]-gridPoints[index])/gridStep;
+					speciesDensity[index+1]+=(species.position[i]-gridPoints[index])/gridStep;
+				}
+				else
+				{
+					speciesDensity[index]+=1-(species.position[i]-gridPoints[index])/gridStep;
+					speciesDensity[0]+=(species.position[i]-gridPoints[index])/gridStep;
+				}
+			}
+			for(int i=0; i<gridPointNumber; i++)
+			{
+				density[i]+=speciesDensity[i]*species.charge;
+			}
+		}
+//        self.freq=self.L*fftfreq(NG, self.dX)
+//        self.freq[0]=0.01
+//        self.pot = np.real(ifft(fft(self.density)[0:NG]/self.freq[0:NG]**2/4./pi**2/EPSILON0))
+//        self.efield = -np.gradient(self.pot)
+//        self.efield+=externalfield(self.X)
+		
+	}
+	
 	public Grid() {
-		// TODO Auto-generated constructor stub
+		gridPoints = new double[gridPointNumber];
+		eField = new double[gridPointNumber];
+		density = new double[gridPointNumber];
+		frequencies = new double[gridPointNumber];
+		potential = new double[gridPointNumber];
 	}
 
 }
--- JavaPIC/src/pic/Species.java
+++ JavaPIC/src/pic/Species.java
@@ -25,9 +25,9 @@ public class Species {
 	
 	public void move(double dt, Grid grid)
 	{
-		for (int i=0; i<this.numberOfParticles; i++)
+		for (int i=0; i<numberOfParticles; i++)
 		{
-			this.position[i] += this.velocity[i]*dt; //move particle
+			position[i] += velocity[i]*dt; //move particle
 			//enforce boundary conditions
 			
 			//suppose size=3, position = 7
@@ -37,8 +37,8 @@ public class Species {
 			//suppose position = -2
 			//howManyPushes = floor (-2/3) = -1
 			//particle gets pushed forward by 1*3=3, to +1
-			double howManyPushesNeeded=Math.floor(this.position[i]/grid.gridSize);
-			this.position[i]-=howManyPushesNeeded*grid.gridSize;
+			double howManyPushesNeeded=Math.floor(position[i]/grid.gridSize);
+			position[i]-=howManyPushesNeeded*grid.gridSize;
 		}
 	}
 	
@@ -46,24 +46,24 @@ public class Species {
 	{
 		int index;
 		double field;
-		for (int i=0; i<this.numberOfParticles; i++)
+		for (int i=0; i<numberOfParticles; i++)
 		{
-			index = grid.getIndexOnGrid(this.position[i]);
+			index = grid.getIndexOnGrid(position[i]);
 			if (index<grid.gridPointNumber-1)
 			{	
 				//interpolate from left
-				field = (grid.gridPoints[index+1]-this.position[i])*grid.eField[i];
+				field = (grid.gridPoints[index+1]-position[i])*grid.eField[i];
 				//interpolate from right
-				field += (this.position[i]-grid.gridPoints[index])*grid.eField[i+1];
+				field += (position[i]-grid.gridPoints[index])*grid.eField[i+1];
 			}
 			else
 			{
 				//interpolate from left
-				field = (-this.position[i])*grid.eField[grid.gridPointNumber-1];
+				field = (-position[i])*grid.eField[grid.gridPointNumber-1];
 				//interpolate from right
-				field += (grid.gridPoints[grid.gridPointNumber-1]-this.position[i])*grid.eField[0];
+				field += (grid.gridPoints[grid.gridPointNumber-1]-position[i])*grid.eField[0];
 			}
-			this.velocity[i]+=field*dt/this.mass*this.charge/grid.gridStep; //time charge for field;
+			velocity[i]+=field*dt/mass*charge/grid.gridStep; //time charge for field;
 											//over mass for accel; over gridstep from interpolation
 		}
 	}
@@ -71,19 +71,19 @@ public class Species {
 	public double temperature()
 	{
 		double averageVel=0;
-		for (int i=0; i<this.numberOfParticles; i++)
+		for (int i=0; i<numberOfParticles; i++)
 		{
-			averageVel+=this.velocity[i]*this.velocity[i];
+			averageVel+=velocity[i]*velocity[i];
 		}
-		averageVel=Math.sqrt(averageVel/this.numberOfParticles);
-		return this.mass*averageVel;
+		averageVel=Math.sqrt(averageVel/numberOfParticles);
+		return mass*averageVel;
 
 	}
 	public void step(Grid grid)
 	{
 		//performs one simulation step for a species 
-		this.accelerate(Parameters.timeStep, grid);
-		this.move(Parameters.timeStep, grid);
+		accelerate(Parameters.timeStep, grid);
+		move(Parameters.timeStep, grid);
 		//TODO: gather trajectory data
 		//TODO: gather velocity data
 		//TODO: gather temperature data
