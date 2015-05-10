1eb527a91fee3b63939213c03580bc4a904c035c
Dominik <stanczakdominik@gmail.com>
2015-05-10 19:40:27 +0200

    Numerical model possibly actually working

--- JavaPIC/src/pic/MainSimulation.java
+++ JavaPIC/src/pic/MainSimulation.java
@@ -9,28 +9,27 @@ public class MainSimulation {
 	
 	public static void main(String[] args) {
 		System.out.println("Starting");
-		double[] protonPositions, protonVelocities, electronPositions, electronVelocities;
 		Grid grid = new Grid();
 		Species[] listOfSpecies = new Species[2];
-		protonPositions=Parameters.uniformPositions(Parameters.numberOfParticles, 0, Parameters.gridSize, 0.25);
-		electronPositions=Parameters.uniformPositions(Parameters.numberOfParticles, 0, Parameters.gridSize, 0.75);
-		protonVelocities=Parameters.uniformVelocity(Parameters.numberOfParticles, 1d);
-		electronVelocities=Parameters.uniformVelocity(Parameters.numberOfParticles, -1d);		
 		
-		Species protons = new Species("Protons", Color.RED, Parameters.numberOfParticles, Parameters.protonRestMass, Parameters.electronCharge, protonPositions, protonVelocities);
-		Species electrons = new Species("Electrons", Color.GREEN, Parameters.numberOfParticles, Parameters.electronRestMass, -Parameters.electronCharge, electronPositions, electronVelocities);
-		listOfSpecies[0]=protons;
-		listOfSpecies[1]=electrons;
-		for (int i=0; i<Parameters.numberOfParticles; i++)
-		{
-			//System.out.println(protons.position[i]);
-		}
+		double[] velocities1=Parameters.uniformVelocity(Parameters.numberOfParticles, Parameters.initialVelocity);
+		double[] velocities2=Parameters.uniformVelocity(Parameters.numberOfParticles, -Parameters.initialVelocity);		
+		double[] positions1=Parameters.cosinePosition(Parameters.numberOfParticles, Parameters.perturbationAmplitude);	
+		double[] positions2=Parameters.cosinePosition(Parameters.numberOfParticles, -Parameters.perturbationAmplitude);
 		
-		Parameters.checkParameters();
-
+		Species beam1 = new Species("Beam 1", Color.RED, Parameters.numberOfParticles/2, Parameters.protonRestMass, Parameters.charge, positions1, velocities1);
+		Species beam2 = new Species("Beam 2", Color.GREEN, Parameters.numberOfParticles/2, Parameters.electronRestMass, Parameters.charge, positions2, velocities2);
+		listOfSpecies[0]=beam1;
+		listOfSpecies[1]=beam2;
+		
+		//Parameters.checkParameters();
+		System.out.println("gridstep " + Parameters.gridStep);
+		System.out.println("charge " + Parameters.charge);
+		System.out.println("bg " + Parameters.backgroundCharge);
+		System.out.println("number" + 2*Parameters.numberOfParticles);
 		grid.update(listOfSpecies);
 		for(int t=0; t<Parameters.iterations; t++)
-		{			
+		{	System.out.println("Iteration " + t);
 			for(int i=0; i<2; i++)
 			{
 				listOfSpecies[i].step(grid);
