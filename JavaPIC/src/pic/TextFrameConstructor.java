70b6adfa8d02dd2771fb8e62a4763c7f2a23182f
Dominik <stanczakdominik@gmail.com>
2015-06-04 22:35:02 +0200

    Funkcjonalność zawijania tekstu

--- JavaPIC/src/pic/TextFrameConstructor.java
+++ JavaPIC/src/pic/TextFrameConstructor.java
@@ -2,8 +2,6 @@ package pic;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.*;
-import java.util.Scanner;
 
 /**
  * Author MATEUSZ
@@ -19,7 +17,6 @@ public class TextFrameConstructor extends JFrame {
             "To sprawia, że fizyka plazmy od strony teoretycznej przechodzi przez elektrodynamikę, zahacza o układy złożone, kradnie połowę pytań z kolokwium z mechaniki płynów wywołując przerażenie u studentów MEiLu, przebiera się za teorię chaosu, a ostatecznie wywraca energetykę do góry nogami - ale to historia na inny wieczór.";
 
 
-
     public TextFrameConstructor(String frameTitle) throws HeadlessException {
         setTitle(frameTitle);
         setSize(540, 600);
@@ -28,16 +25,15 @@ public class TextFrameConstructor extends JFrame {
 
         JTextArea text = null;
 
-        if(frameTitle.equals("Fizyka Plazmy")){
+        if (frameTitle.equals("Fizyka Plazmy")) {
             text = new JTextArea(plazma);
         }
         add(text);
 
 
-        //funkcjonalnoĹ›Ä‡ wyĹ›wietlania tekstu informacyjnego
         text.setFont(new Font("Sans Serif", Font.PLAIN, 16));
-        text.setLineWrap(true); //zawijanie czcionek
-        text.setEditable(false); //wyĹ‚Ä…cza edycjÄ™ tekstu
+        text.setLineWrap(true);
+        text.setEditable(false);
         text.setVisible(true);
         text.setWrapStyleWord(true);
     }
